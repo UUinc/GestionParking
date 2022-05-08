ca4ecd54c773c2ec0e5d7ff2ece115e5270dde5e
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-05-08 14:42:41 +0100

    home page desgin

--- Accueil.Designer.vb
+++ Accueil.Designer.vb
@@ -23,68 +23,142 @@ Partial Class Accueil
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Title_label = New System.Windows.Forms.Label()
-        Me.gestionVoiture_button = New System.Windows.Forms.Button()
-        Me.gestionCamions_button = New System.Windows.Forms.Button()
-        Me.GestionUtilisateurs_button = New System.Windows.Forms.Button()
+        Me.carManagement_button = New System.Windows.Forms.Button()
+        Me.truckManagement_button = New System.Windows.Forms.Button()
+        Me.userManagement_button = New System.Windows.Forms.Button()
+        Me.titlebar_panel = New System.Windows.Forms.Panel()
+        Me.logout_button = New System.Windows.Forms.Button()
+        Me.viewUsers_button = New System.Windows.Forms.Button()
+        Me.titlebar_panel.SuspendLayout()
         Me.SuspendLayout()
         '
         'Title_label
         '
         Me.Title_label.AutoSize = True
-        Me.Title_label.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Title_label.Location = New System.Drawing.Point(152, 45)
+        Me.Title_label.BackColor = System.Drawing.Color.Transparent
+        Me.Title_label.Font = New System.Drawing.Font("Kanit Medium", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Title_label.ForeColor = System.Drawing.Color.White
+        Me.Title_label.Location = New System.Drawing.Point(60, 20)
         Me.Title_label.Name = "Title_label"
-        Me.Title_label.Size = New System.Drawing.Size(500, 32)
+        Me.Title_label.Size = New System.Drawing.Size(256, 68)
         Me.Title_label.TabIndex = 0
-        Me.Title_label.Text = "Bienvenue dans l' application gestion parking"
+        Me.Title_label.Text = "Home page"
         '
-        'gestionVoiture_button
+        'carManagement_button
         '
-        Me.gestionVoiture_button.Location = New System.Drawing.Point(12, 154)
-        Me.gestionVoiture_button.Name = "gestionVoiture_button"
-        Me.gestionVoiture_button.Size = New System.Drawing.Size(209, 125)
-        Me.gestionVoiture_button.TabIndex = 1
-        Me.gestionVoiture_button.Text = "Gestion Voitures"
-        Me.gestionVoiture_button.UseVisualStyleBackColor = True
+        Me.carManagement_button.BackColor = System.Drawing.Color.Transparent
+        Me.carManagement_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.carManagement_button.FlatAppearance.BorderSize = 0
+        Me.carManagement_button.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.carManagement_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.carManagement_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.carManagement_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.carManagement_button.Image = Global.GestionParking.My.Resources.Resources.carManagement_btn
+        Me.carManagement_button.Location = New System.Drawing.Point(39, 105)
+        Me.carManagement_button.Name = "carManagement_button"
+        Me.carManagement_button.Size = New System.Drawing.Size(450, 280)
+        Me.carManagement_button.TabIndex = 1
+        Me.carManagement_button.UseVisualStyleBackColor = False
         '
-        'gestionCamions_button
+        'truckManagement_button
         '
-        Me.gestionCamions_button.Location = New System.Drawing.Point(297, 154)
-        Me.gestionCamions_button.Name = "gestionCamions_button"
-        Me.gestionCamions_button.Size = New System.Drawing.Size(209, 125)
-        Me.gestionCamions_button.TabIndex = 2
-        Me.gestionCamions_button.Text = "Gestion Camions"
-        Me.gestionCamions_button.UseVisualStyleBackColor = True
+        Me.truckManagement_button.BackColor = System.Drawing.Color.Transparent
+        Me.truckManagement_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.truckManagement_button.FlatAppearance.BorderSize = 0
+        Me.truckManagement_button.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.truckManagement_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.truckManagement_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.truckManagement_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.truckManagement_button.Image = Global.GestionParking.My.Resources.Resources.truckManagement_btn
+        Me.truckManagement_button.Location = New System.Drawing.Point(39, 398)
+        Me.truckManagement_button.Name = "truckManagement_button"
+        Me.truckManagement_button.Size = New System.Drawing.Size(450, 280)
+        Me.truckManagement_button.TabIndex = 2
+        Me.truckManagement_button.UseVisualStyleBackColor = False
         '
-        'GestionUtilisateurs_button
+        'userManagement_button
         '
-        Me.GestionUtilisateurs_button.Location = New System.Drawing.Point(579, 154)
-        Me.GestionUtilisateurs_button.Name = "GestionUtilisateurs_button"
-        Me.GestionUtilisateurs_button.Size = New System.Drawing.Size(209, 125)
-        Me.GestionUtilisateurs_button.TabIndex = 3
-        Me.GestionUtilisateurs_button.Text = "Gestion Utilisateurs"
-        Me.GestionUtilisateurs_button.UseVisualStyleBackColor = True
+        Me.userManagement_button.BackColor = System.Drawing.Color.Transparent
+        Me.userManagement_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.userManagement_button.FlatAppearance.BorderSize = 0
+        Me.userManagement_button.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.userManagement_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.userManagement_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.userManagement_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.userManagement_button.Image = Global.GestionParking.My.Resources.Resources.userManagement_btn
+        Me.userManagement_button.Location = New System.Drawing.Point(516, 118)
+        Me.userManagement_button.Name = "userManagement_button"
+        Me.userManagement_button.Size = New System.Drawing.Size(345, 540)
+        Me.userManagement_button.TabIndex = 3
+        Me.userManagement_button.UseVisualStyleBackColor = False
+        '
+        'titlebar_panel
+        '
+        Me.titlebar_panel.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.titlebar_panel.Controls.Add(Me.logout_button)
+        Me.titlebar_panel.Controls.Add(Me.Title_label)
+        Me.titlebar_panel.Location = New System.Drawing.Point(0, 0)
+        Me.titlebar_panel.Name = "titlebar_panel"
+        Me.titlebar_panel.Size = New System.Drawing.Size(1270, 90)
+        Me.titlebar_panel.TabIndex = 4
+        '
+        'logout_button
+        '
+        Me.logout_button.BackColor = System.Drawing.Color.Transparent
+        Me.logout_button.BackgroundImage = Global.GestionParking.My.Resources.Resources.logout
+        Me.logout_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.logout_button.FlatAppearance.BorderSize = 0
+        Me.logout_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.logout_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.logout_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.logout_button.Location = New System.Drawing.Point(1176, 37)
+        Me.logout_button.Name = "logout_button"
+        Me.logout_button.Size = New System.Drawing.Size(50, 50)
+        Me.logout_button.TabIndex = 1
+        Me.logout_button.UseVisualStyleBackColor = False
+        '
+        'viewUsers_button
+        '
+        Me.viewUsers_button.BackColor = System.Drawing.Color.Transparent
+        Me.viewUsers_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.viewUsers_button.FlatAppearance.BorderSize = 0
+        Me.viewUsers_button.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.viewUsers_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.viewUsers_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.viewUsers_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.viewUsers_button.Image = Global.GestionParking.My.Resources.Resources.viewUsers_btn
+        Me.viewUsers_button.Location = New System.Drawing.Point(881, 118)
+        Me.viewUsers_button.Name = "viewUsers_button"
+        Me.viewUsers_button.Size = New System.Drawing.Size(345, 540)
+        Me.viewUsers_button.TabIndex = 5
+        Me.viewUsers_button.UseVisualStyleBackColor = False
         '
         'Accueil
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1264, 690)
-        Me.Controls.Add(Me.GestionUtilisateurs_button)
-        Me.Controls.Add(Me.gestionCamions_button)
-        Me.Controls.Add(Me.gestionVoiture_button)
-        Me.Controls.Add(Me.Title_label)
+        Me.Controls.Add(Me.viewUsers_button)
+        Me.Controls.Add(Me.titlebar_panel)
+        Me.Controls.Add(Me.userManagement_button)
+        Me.Controls.Add(Me.truckManagement_button)
+        Me.Controls.Add(Me.carManagement_button)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.Name = "Accueil"
-        Me.Text = "Accueil"
+        Me.Text = "Home page"
+        Me.titlebar_panel.ResumeLayout(False)
+        Me.titlebar_panel.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Title_label As Label
-    Friend WithEvents gestionVoiture_button As Button
-    Friend WithEvents gestionCamions_button As Button
-    Friend WithEvents GestionUtilisateurs_button As Button
+    Friend WithEvents carManagement_button As Button
+    Friend WithEvents truckManagement_button As Button
+    Friend WithEvents userManagement_button As Button
+    Friend WithEvents titlebar_panel As Panel
+    Friend WithEvents viewUsers_button As Button
+    Friend WithEvents logout_button As Button
 End Class
--- Accueil.vb
+++ Accueil.vb
@@ -1,7 +1,13 @@
 ﻿Public Class Accueil
-    Private Sub gestionVoiture_button_Click(sender As Object, e As EventArgs) Handles gestionVoiture_button.Click
+    Private Sub gestionVoiture_button_Click(sender As Object, e As EventArgs) Handles carManagement_button.Click
         GestionVoiture.Show()
         GestionVoiture.Location = Location
+        Hide()
+    End Sub
+
+    Private Sub logout_button_Click(sender As Object, e As EventArgs) Handles logout_button.Click
+        Login.Show()
+        Login.Location = Location
         Hide()
     End Sub
 End Class
