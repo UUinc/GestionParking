999b5afa029069ddd85d6198d3cd71878b051f0a
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-05-08 15:13:14 +0100

    Revert "home page desgin"
    This reverts commit ca4ecd54c773c2ec0e5d7ff2ece115e5270dde5e.

--- Accueil.vb
+++ Accueil.vb
@@ -1,13 +1,7 @@
 ﻿Public Class Accueil
-    Private Sub gestionVoiture_button_Click(sender As Object, e As EventArgs) Handles carManagement_button.Click
+    Private Sub gestionVoiture_button_Click(sender As Object, e As EventArgs) Handles gestionVoiture_button.Click
         GestionVoiture.Show()
         GestionVoiture.Location = Location
-        Hide()
-    End Sub
-
-    Private Sub logout_button_Click(sender As Object, e As EventArgs) Handles logout_button.Click
-        Login.Show()
-        Login.Location = Location
         Hide()
     End Sub
 End Class
